7f0b999bb90d9a3c912fe382cf983a92f2829d98
icguanyu <icguanyu@gmail.com>
2018-09-18 14:38:30 +0800

    assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/VuecliPractice',
     proxyTable: {},
 
     // Various Dev Server settings
